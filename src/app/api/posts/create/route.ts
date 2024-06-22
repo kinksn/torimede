a9d56e4c8d9927a80c42a691af730e33f8b73f5a
kinksn <kataoka0417@gmail.com>
2024-06-22 21:03:31 +0900

    [TAS-16] PostにUserをリレーショナル

--- src/app/api/posts/create/route.ts
+++ src/app/api/posts/create/route.ts
@@ -1,14 +1,20 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
+import { getAuthSession } from "@/lib/auth";
 
 export async function POST(req: Request) {
   try {
+    const session = await getAuthSession();
+    if (!session?.user) {
+      return NextResponse.json({ message: "not login" }, { status: 500 });
+    }
     const body = await req.json();
     const post = await db.post.create({
       data: {
         title: body.title,
         content: body.content,
         tagId: body.tagId || null,
+        userId: session.user.id,
       },
     });
     return NextResponse.json(post, { status: 200 });
