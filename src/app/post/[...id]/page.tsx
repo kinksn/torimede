999a719e1a5ce0a64b208c06bbc668d7475d51b9
shunya kataoka <kataoka0417@gmail.com>
2024-09-07 19:37:42 +0900

    [TAS-44] 40x,50x,aboutページの実装

--- src/app/post/[...id]/page.tsx
+++ src/app/post/[...id]/page.tsx
@@ -195,6 +195,7 @@ const BlogDetailPage: FC<PostProps> = async ({ params }) => {
         <ShareButtons text={post.title} />
         <UrlCopyButton />
       </div>
+      <h2 className="text-1xl font-bold my-4">{userName}の投稿</h2>
       {userPost.map((post) => (
         <PostCard post={post} key={post.id} />
       ))}
