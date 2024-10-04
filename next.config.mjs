2f55f819573de20cad168f262f495bb0f128e4d9
shunya kataoka <kataoka0417@gmail.com>
2024-10-05 02:08:15 +0900

    prodの画像を読み込めるように設定追加

--- next.config.mjs
+++ next.config.mjs
@@ -5,17 +5,22 @@ const nextConfig = {
       // S3
       {
         protocol: "https",
-        hostname: `pp-web-strage.s3.ap-southeast-2.amazonaws.com`,
+        hostname: "pp-web-strage.s3.ap-southeast-2.amazonaws.com",
       },
       // X
       {
         protocol: "https",
-        hostname: `pbs.twimg.com`,
+        hostname: "pbs.twimg.com",
       },
       // Google
       {
         protocol: "https",
-        hostname: `lh3.googleusercontent.com`,
+        hostname: "lh3.googleusercontent.com",
+      },
+      // prod
+      {
+        protocol: "https",
+        hostname: "torimede.vercel.app",
       },
     ],
   },
