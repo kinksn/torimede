13807c3db5f2a17e7728c20c027cb16b9d6bdb9f
shunya kataoka <kataoka0417@gmail.com>
2025-01-15 22:15:11 +0900

    [TAS-72] メインビジュアルの実装（静的）

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default async function RootLayout({
       <body>
         <Providers>
           <Header initialSession={session} profileImage={profileImage} />
-          <div className="container h-full pt-12">{children}</div>
+          {children}
           {modal}
           <Footer />
         </Providers>
