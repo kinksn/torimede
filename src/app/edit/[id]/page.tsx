2740a6a71d713d0207c5a0b99cb8876d992f3dcc
shunya kataoka <kataoka0417@gmail.com>
2025-02-07 13:27:06 +0900

    投稿詳細ページのタグをidの降順で取得するように

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,6 +1,7 @@
 import EditPostPage from "@/app/edit/[id]/EditPostPage";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { orderBy } from "es-toolkit";
 import React from "react";
 
 type EditPostPageProps = {
@@ -11,7 +12,11 @@ type EditPostPageProps = {
 
 const Edit = async ({ params }: EditPostPageProps) => {
   const session = await getAuthSession();
-  const tags = await db.tag.findMany();
+  const tags = await db.tag.findMany({
+    orderBy: {
+      id: "desc",
+    },
+  });
   return <EditPostPage id={params.id} tags={tags} session={session} />;
 };
 
