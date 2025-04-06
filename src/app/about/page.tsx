ee06a0934f5f311e6e8d659b6dbdefe90568332c
shunya kataoka <kataoka0417@gmail.com>
2025-04-06 14:17:19 +0900

    [TAS-117] aboutページのアップデート

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Check from "@/components/assets/icon/check.svg";
 import { ContentToolbar } from "@/components/ContentToolbar";
 import type { Metadata } from "next";
 import { cn } from "@/lib/utils";
@@ -7,6 +8,7 @@ import {
   DESCRIPTION,
   METADATA_TITLE,
 } from "@/app/shared-metadata";
+import { SVGIcon } from "@/components/ui/SVGIcon";
 
 export const metadata: Metadata = {
   title: METADATA_TITLE.about,
@@ -24,108 +26,174 @@ export default function About() {
     <>
       <ContentToolbar />
       <article className="w-full px-5 flex flex-col items-center justify-center">
+        <header>
+          <div className="flex items-center max-sm:flex-col gap-5 mt-10 mb-10">
+            <div className="flex flex-col gap-2">
+              <h1 className="font-zenMaruGothic text-primary-700 text-typography-xxxl max-sm:text-typography-xxl font-bold max-sm:font-bold max-sm:leading-[1.8] flex flex-col text-left max-sm:items-center max-sm:text-center">
+                <p>
+                  鳥さんを
+                  <span className={medeTextGradientStyle}>愛でて</span>
+                </p>
+                <p>いやしの輪をひろげる</p>
+              </h1>
+              <p>
+                くわしい人から普段なんとなく鳥さんが気になっている人まで、
+                <br className="max-sm:hidden" />
+                すべての鳥好きが楽しめる場所です。
+              </p>
+            </div>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/kv.svg`}
+              className="max-w-[190px] max-sm:max-w-[148px]"
+              alt="メデちゃんがハートを出して喜んでいる姿"
+              width={190}
+              height={190}
+              layout="responsive"
+              loading="lazy"
+            />
+          </div>
+        </header>
+        <Section title="楽しみ方" subTitle="how">
+          <div className="flex flex-col items-center gap-[60px] max-sm:gap-10 mt-20">
+            <ul className="flex justify-center max-sm:flex-col gap-10 max-sm:gap-20">
+              <li className="relative flex flex-col items-center max-w-[380px] w-full rounded-20 bg-white pt-20 pb-10">
+                <small className="flex justify-center absolute h-20 top-0 left-0 right-0 mx-auto -translate-y-[50%]">
+                  <span className="flex items-center justify-center bg-secondary-50 rounded-full h-full w-fit px-10">
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/how-title01.svg`}
+                      alt="楽しみ方その1"
+                      width={56}
+                      height={21}
+                      className="min-w-[56px]"
+                    />
+                  </span>
+                </small>
+                <div className="px-7">
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/how-01.png`}
+                    alt="トリメデで投稿をするイメージ画像"
+                    width={1296}
+                    height={936}
+                    layout="responsive"
+                    loading="lazy"
+                  />
+                </div>
+                <div className="flex flex-col gap-5 items-center px-10 pt-10">
+                  <h2 className="font-zenMaruGothic text-typography-lg font-bold leading-none">
+                    投稿する
+                  </h2>
+                  <p className="font-medium">
+                    街中の野鳥さん、ペットの鳥さんなど、日常で出会う鳥さんを投稿しましょう。イラストやグッズの鳥さんでもOKです。
+                  </p>
+                </div>
+              </li>
+              <li className="relative flex flex-col items-center max-w-[380px] w-full rounded-20 bg-white pt-20 pb-10">
+                <small className="flex justify-center absolute h-20 top-0 left-0 right-0 mx-auto -translate-y-[50%]">
+                  <span className="flex items-center justify-center bg-secondary-50 rounded-full h-full w-fit px-10">
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/how-title02.svg`}
+                      alt="楽しみ方その2"
+                      width={56}
+                      height={21}
+                      className="min-w-[56px]"
+                    />
+                  </span>
+                </small>
+                <div className="px-7">
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/how-02.png`}
+                    alt="メデボタンをおして投稿された鳥さんを愛でるイメージ画像"
+                    width={1296}
+                    height={936}
+                    layout="responsive"
+                    loading="lazy"
+                  />
+                </div>
+                <div className="flex flex-col gap-5 items-center px-10 pt-10">
+                  <h2 className="font-zenMaruGothic text-typography-lg font-bold leading-none">
+                    愛でる
+                  </h2>
+                  <p className="font-medium">
+                    好きな鳥さんを選んで「メデボタン」を押すと愛でることができます。SNSの「いいね」とはちがい数値は表示されません。
+                  </p>
+                </div>
+              </li>
+              <li className="relative flex flex-col items-center max-w-[380px] w-full rounded-20 bg-white pt-20 pb-10">
+                <small className="flex justify-center absolute h-20 top-0 left-0 right-0 mx-auto -translate-y-[50%]">
+                  <span className="flex items-center justify-center bg-secondary-50 rounded-full h-full w-fit px-10">
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/how-title03.svg`}
+                      alt="楽しみ方その3"
+                      width={56}
+                      height={21}
+                      className="min-w-[56px]"
+                    />
+                  </span>
+                </small>
+                <div className="px-7">
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/how-03.png`}
+                    alt="キラキラになった投稿のイメージ画像"
+                    width={1296}
+                    height={936}
+                    layout="responsive"
+                    loading="lazy"
+                  />
+                </div>
+                <div className="flex flex-col gap-5 items-center px-10 pt-10">
+                  <h2 className="font-zenMaruGothic text-typography-lg font-bold leading-none">
+                    集める
+                  </h2>
+                  <p className="font-medium">
+                    自分が投稿した鳥さんがたくさん愛でられるとキラキラ輝く特別なカードになります。カードはマイページで見ることができます。
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </Section>
         <Section
-          title={
+          title="SNS疲れさせません"
+          subTitle="promise"
+          description={
             <>
-              トリメデは
-              <br className="hidden max-sm:block" />
-              鳥さんを
-              <span className={medeTextGradientStyle}>愛でる</span>
-              ための
+              気を使うことなく楽しめるように、
               <br />
-              画像共有サービスです
+              以下の約束を守ります
             </>
           }
-          className="pt-0"
-          subTitle="about"
         >
-          <p className="mt-5 text-center max-sm:text-left">
-            くわしい人から普段なんとなく鳥さんが気になっている人まで、
-            <br className="max-sm:hidden" />
-            すべての鳥好きが楽しめる場所です。
-          </p>
-          <Image
-            src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/kv.svg`}
-            className="max-w-[190px] max-sm:max-w-[148px] mt-10"
-            alt="メデちゃんがハートを出して喜んでいる姿"
-            width={190}
-            height={190}
-            layout="responsive"
-            loading="lazy"
-          />
-        </Section>
-        <Section title="トリメデの楽しみ方" subTitle="how">
-          <div className="flex flex-col items-center gap-[60px] max-sm:gap-10 mt-10 max-sm:mt-5">
-            <div className="flex flex-col items-center gap-5">
-              <h2 className="font-zenMaruGothic text-typography-xl font-bold text-tertialy-oceanblue-400">
-                <span className="text-[24px]">1.</span>投稿する
+          <ul className="flex justify-center max-sm:flex-col mt-10 gap-10 max-w-[1280px]">
+            <li className="flex flex-col gap-1 p-10 rounded-20 bg-secondary-50 border-4 border-secondary-100">
+              <h2 className="flex items-center gap-2 text-tertialy-oceanblue-400 font-bold text-typography-lg font-zenMaruGothic">
+                <div className="flex items-center justify-center rounded-full w-6 h-6 bg-tertialy-oceanblue-400">
+                  <SVGIcon svg={Check} className="w-5 text-base-content" />
+                </div>
+                通知しない
               </h2>
-              <Image
-                src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/post.png`}
-                className={"max-w-[440px]"}
-                alt="トリメデで投稿をするイメージ画像"
-                width={1768}
-                height={1216}
-                layout="responsive"
-                loading="lazy"
-              />
-              <p>
-                鳥さんに関係するものならなんでもOKです。
-                <br className="max-sm:hidden" />
-                街中で見つけた野鳥、自宅に遊びに来る鳥さん、ペットの鳥さんなど、
-                <br className="max-sm:hidden" />
-                日常で出会う鳥さんを投稿して一緒に楽しみましょう。
-                <br className="max-sm:hidden" />
-                また、本物の鳥さんだけでなくイラストやグッズの写真もOKです。
-                <br className="max-sm:hidden" />
-                投稿するとみんなが見れるタイムラインに表示されます。
-              </p>
-            </div>
-            <div className="flex flex-col items-center gap-5">
-              <h2
-                className={`font-zenMaruGothic text-typography-xl font-bold text-tertialy-oceanblue-400 ${medeTextGradientStyle}`}
-              >
-                <span className="text-[24px]">2.</span>愛でる
+              <p>あなたが愛でた鳥さんを投稿者や他の人に知らせません。</p>
+            </li>
+            <li className="flex flex-col gap-1 p-10 rounded-20 bg-secondary-50 border-4 border-secondary-100">
+              <h2 className="flex items-center gap-2 text-tertialy-oceanblue-400 font-bold text-typography-lg font-zenMaruGothic">
+                <div className="flex items-center justify-center rounded-full w-6 h-6 bg-tertialy-oceanblue-400">
+                  <SVGIcon svg={Check} className="w-5 text-base-content" />
+                </div>
+                数値化しない
               </h2>
-              <Image
-                src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/mede.png`}
-                className={"max-w-[576px] max-sm:hidden"}
-                alt="投稿された鳥さんの画像のメデボタンを押しているイメージ画像（PC用）"
-                width={2304}
-                height={1520}
-                loading="lazy"
-              />
-              <Image
-                src={`${process.env.NEXT_PUBLIC_ASSET_BASE_URL}/image/about/mede-sp.png`}
-                className={"hidden max-sm:block"}
-                alt="投稿された鳥さんの画像のメデボタンを押しているイメージ画像（SP用）"
-                width={1340}
-                height={880}
-                layout="responsive"
-                loading="lazy"
-              />
+              <p>愛でた回数やあなたの行動を数値化して他の人に見せません。</p>
+            </li>
+            <li className="flex flex-col gap-1 p-10 rounded-20 bg-secondary-50 border-4 border-secondary-100">
+              <h2 className="flex items-center gap-2 text-tertialy-oceanblue-400 font-bold text-typography-lg font-zenMaruGothic">
+                <div className="flex items-center justify-center rounded-full w-6 h-6 bg-tertialy-oceanblue-400">
+                  <SVGIcon svg={Check} className="w-5 text-base-content" />
+                </div>
+                勝手に公開しない
+              </h2>
               <p>
-                好きな鳥さんを選んで「メデボタン」を押すと愛でることができます。
-                <br className="max-sm:hidden" />
-                SNSの「いいね」とはちがい数値は表示されません。
-                <br className="max-sm:hidden" />
-                愛でてもらった鳥さんは幸せな変化が起こるかもしれないので、
-                <br className="max-sm:hidden" />
-                気になった鳥さんをたくさん愛でてみましょう。
+                育った鳥さんなどあなたが手に入れたものを他の人に見せません。
               </p>
-            </div>
-          </div>
-        </Section>
-        <Section title="SNS疲れはありません" subTitle="promise">
-          <div className="rounded-20 bg-white p-10 mt-10">
-            <p>
-              トリメデは投稿と愛でることしかできません。
-              <br className="max-sm:hidden" />
-              メデボタンを押しても誰が愛でたか分かるような通知が飛ぶことはないので、
-              <br className="max-sm:hidden" />
-              気を使うことなく好きなだけ鳥さんを愛でたり投稿したり楽しんでください。
-            </p>
-          </div>
+            </li>
+          </ul>
         </Section>
       </article>
     </>
@@ -136,12 +204,14 @@ const Section = ({
   children,
   title,
   subTitle,
+  description,
   className,
   gap,
 }: {
   children: React.ReactNode;
   title: React.ReactNode | string;
-  subTitle: string;
+  subTitle?: string;
+  description?: React.ReactNode;
   className?: string;
   gap?: number;
 }) => {
@@ -155,10 +225,17 @@ const Section = ({
       )}
     >
       <header className="flex flex-col items-center justify-center">
-        <span className="font-comfortaa text-secondary-400">{subTitle}</span>
-        <h1 className="font-zenMaruGothic text-primary-700 text-center text-typography-xxxl max-sm:text-typography-xxl font-bold max-sm:font-bold max-sm:leading-[1.8]">
+        {subTitle && (
+          <span className="font-comfortaa text-secondary-400 leading-none">
+            {subTitle}
+          </span>
+        )}
+        <h1 className="font-zenMaruGothic text-primary-700 text-center text-typography-xxl max-sm:text-typography-xxl font-bold max-sm:font-bold max-sm:leading-[1.6]">
           {title}
         </h1>
+        {description && (
+          <p className="text-typography-sm mt-1 text-center">{description}</p>
+        )}
       </header>
       {children}
     </section>
